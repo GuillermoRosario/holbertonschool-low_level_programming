fe4f34de6f3fb68fa5cae9c15c6897235b0b2e95
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-02 20:15:50 +0000

    name

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /** 
- * Hello printf 
- */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
 
-int main() 
+int main(void) 
 
 {
-       	printf( "with proper grammar, but the outcome is a piece of art,\n" );
-
-} 
+       	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0)
+
+}
