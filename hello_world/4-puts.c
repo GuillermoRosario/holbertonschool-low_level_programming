f85ec155c4481ed29465e6c7515f336b9d950b45
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-02 19:56:46 +0000

    name

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
-int main()
-{ 
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-
-}
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);}
