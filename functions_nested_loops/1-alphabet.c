878187f5fc8b6a72b86005a45209779d13f6c088
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-05 20:13:50 +0000

    putchar

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,19 +1,16 @@
-#include "s"
+#include "main.h"
 /**
- * print_alphabet - Entry point
- *
+ * print_alphabet - Print _putchar
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-	int alpha;
+	int ii;
 
-	alpha = 'a';
-
-	while (alpha <= 'z')
+	for (ii = 'a'; ii <= 'z'; ii++)
 	{
-		_putchar(alpha);
-		alpha++;
+		_putchar(ii);
 	}
+
 	_putchar('\n');
 }
