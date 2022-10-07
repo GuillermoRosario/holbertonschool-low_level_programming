4ee11498662447280db1f959e807fa665b1c4c32
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-07 21:34:04 +0000

    new file

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_numbers - Prints the numbers from 0-9.
@@ -8,6 +9,7 @@ void print_numbers(void)
 	int num;
 
 	for (num = 0; num <= 9; num++)
+	
 		_putchar((num % 10) + '0');
 
 	_putchar('\n');
