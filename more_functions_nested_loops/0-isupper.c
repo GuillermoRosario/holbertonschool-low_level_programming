a1531125d02089f7c15d8692587cb48c516f52e4
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-07 21:01:29 +0000

    new file

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,5 @@
-/*
- * File: 0-isupper.c
- * Auth: Brennan D Baraban
- */
-
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _isupper - Checks for uppercase characters.
@@ -11,6 +7,7 @@
  *
  * Return: 1 if character is uppercase, 0 otherwise.
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
