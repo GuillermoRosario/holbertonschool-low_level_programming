b2d267c0e96e2f95900d62a5868ba2e9686b7d50
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-07 20:56:50 +0000

    Code Fix

---
+++ variables_if_else_while/10_Guillermo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print my name
+ *
+ * Return - Success (0)
+ * 
+ **/
+
+int main()
+{
+	const char *Myname = "Guillermo";
+	int Myage = 35;
+
+	printf("Hello, My name is %s and i'm %d years old\n", "Myname" ,"Myage");
+	
+	return (0);
+}
+		
