d9491ebd175919c41ffba144e5d30d7fde135c7d
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-11 23:31:40 +0000

    Puts

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -5,21 +5,19 @@
 
 #include "main.h"
 #include <stddef.h>
+
 /**
  * _strlen - Returns the length of a string.
  * @s: The string to get the length of.
  *
  * Return: Always 0 The length of @s.
  */
-
  size_t strlen(const char *s)
-
-{	
+{
 	size_t length = 0;
 
 	while (*s++)
 		(length++);
 
 	return (length);
-
 }
