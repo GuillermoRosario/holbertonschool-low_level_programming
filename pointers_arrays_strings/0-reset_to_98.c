e062d0ce05d5bb8d78b25005d62393f711b66d9a
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-11 19:19:39 +0000

    0 pointer and array

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <main.h>
+#include <stdio.h>
+
+/** Main - Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	void reset_to_98(int *n);
+
+int *n;
+int n=98
+
+	int putchar("Value point: %lu)
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this directory we will learn about pointers & arrays in the C Programming language. 
