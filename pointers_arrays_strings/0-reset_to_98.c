0af76f66592d7b354546bbf5e8c6356e1f4afc4a
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-11 19:27:27 +0000

    0 pointer and array

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,20 @@
-#include <main.h>
-#include <stdio.h>
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Guillermo Rosario
+ * */
 
-/** Main - Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
+#include "main.h"
+
+/**
+ *  Reset to 98 - Takes a pointer to an int as parameter 
+ *  		  and updates the value it points to to 98.
  *
  * Return: Always 0
  */
 
-int main(void)
-{
 	void reset_to_98(int *n);
+{
 
-int *n;
-int n=98
+	*n = 98;
+}
 
-	int putchar("Value point: %lu)
