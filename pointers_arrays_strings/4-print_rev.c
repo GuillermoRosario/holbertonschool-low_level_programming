0c472274cd1f301de70a93bf2eb68106a43d89b8
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-12 22:39:07 +0000

    Ejercicio 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,41 @@
+
+/*
+ * File - 4-print_rev.c
+ * Auth - Guillermo Rosario
+ *
+ */
+
+#include "main.h"
+/**
+ * _strlen - find lenght of a string.
+ * @s: string to find the lenght.
+ * Return: string in reverse
+ */
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; s[counter] != '\0';)
+		counter++;
+
+	return (counter);
+}
+/**
+ * print_rev - print string in reverse
+ * @s: reverse to be reverse
+ * Return: String in reverse
+ */
+void print_rev(char *s)
+{
+	int reverse;
+	int len;
+
+	len =  _strlen(s);
+
+	for (reverse = len - 1; reverse >= 0 ; reverse--)
+	{
+		putchar(s[reverse]);
+	}
+
+	putchar('\n');
+}
