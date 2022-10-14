3f6352064e363e00aed5f7cda59323c5800ac7f7
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-14 15:21:52 +0000

    Ejercicio 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,25 @@
 #include "main.h"
-
-/* Write a function that concatenates two strings.*/
-
-char *_strncat(char *dest, char *src, int n);
+/**
+ * *_strncat - concatnate 2 arrays together
+ * @dest: first array
+ * @src: second array.
+ * @n: numbers of char in the array
+ * Return: addition of the values of 2 arrays.
+ */
+char *_strncat(char *dest, char *src, int n)
 {
-int index;
-int move;
+	int i;
+	int j;
 
+	for (i = 0; dest[i] != '\0';)
+	{
+		i++;
+	}
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
