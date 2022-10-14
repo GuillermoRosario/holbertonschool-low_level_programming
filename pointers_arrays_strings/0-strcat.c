d7f9596847bd1430c9b5c0b380de7b7fe0dee280
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-14 16:12:00 +0000

    Ejercicio 1

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,26 @@
 #include "main.h"
-
+/**
+ * *_strcat - concatnate 2 arrays together
+ * @dest: first array
+ * @src: second array.
+ * Return: addition of the values of 2 arrays.
+ */
 char *_strcat(char *dest, char *src)
 {
-int index;
-int move;
+	int i;
+	int j;
 
-	for (index = 0; dest[index] != '\0';)
+	for (i = 0; dest[i] != '\0';)
 	{
-		index++;
+		i++;
 	}
-	for (move = 0; src[move] != '\0'; move++)
+
+	for (j = 0; src[j] != '\0';)
 	{
-		dest[index] = src[move];
-		index++;
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
