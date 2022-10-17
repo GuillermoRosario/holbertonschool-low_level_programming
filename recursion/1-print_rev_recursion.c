1dce6fd2a41a3da09dd254bf8778a59b07f3fff2
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-17 18:30:06 +0000

    Ejercicio 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
-/* Write a function that prints 
+/* Write a function that prints
  * a string in reverse
  *
  * _print_rev_recursion - Print string in reverse
  * @s - string to be printed.
- */
+ * */
 
 void _print_rev_recursion(char *s)
 {
