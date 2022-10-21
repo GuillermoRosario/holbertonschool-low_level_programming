d9e9a4879600258885644b76ac5e28fbdc15e46b
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-21 23:52:14 +0000

    Ejercicio 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-int main (int __attribute__((unused)) argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	int i;
-	for(i = 0; i < argc; i++)
-{
+
+	for (i = 0; i < argc; i++)
+	{
 	printf("%d",*argv[i]);
-}
+	}
 	return 0;
 }
