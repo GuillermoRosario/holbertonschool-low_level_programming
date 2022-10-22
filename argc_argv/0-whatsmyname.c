6e3ebdddd7a0d4fc8eebc229d18ba8f8695f069a
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-22 02:51:56 +0000

    Ejercicio 1

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
-int main (int __attribute__((unused)) argc, char *argv[])
+#include "stdio.h"
+/**
+ * main - print program name
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
 	{
-		printf ("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
+		break;
 	}
-return (0);
+	return (0);
+}
