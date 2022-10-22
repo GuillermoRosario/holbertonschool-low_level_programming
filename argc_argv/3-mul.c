bed3f909cecdc9673e10e0cfd0eb8877fb973025
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-22 03:04:57 +0000

    Ejercicio 1

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - print program name
- * @argc: number of arguments
- * @argv: arguments
- * Return: always 0 (success)
+ * main - get the name of the program
+ * @argv: argument vector, get the amount of arguement.
+ * @argc: argument constant
+ * Return: return sum of two intergers
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-	int x, y, mult;
+	int a, b, mul;
 
-	if (argc <= 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		x = atoi(argv[1]);
-		y = atoi(argv[2]);
-		mult = (x * y);
-		printf("%d\n", mult);
-	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	mul = a * b;
+
+	printf("%d\n", mul);
+
+
 	return (0);
+}
