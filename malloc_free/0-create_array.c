36e4f53917529f467ba48f6b74e5833a62949ac2
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-25 15:14:39 +0000

    Ejercicio 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/** Write a function that creates an array
+/*
+ * Write a function that creates an array
  * of chars, and initializes it with a specific char.
  *
  * Returns Null if size = 0
@@ -13,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int a;
 
-	if(size == 0)
+	if (size == 0)
 		return (NULL);
 	s = malloc(size * sizeof(*s));
 	if (s == NULL)
