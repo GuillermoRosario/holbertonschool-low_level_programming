3e31ae02c9e26619328ad1eac108a7f99cb8b56e
GuillermoRosario <guillermorosario628@gmail.com>
2022-10-27 22:01:15 +0000

    more malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,28 +1,44 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
- *_calloc -allocated memoria for nmeb elemn de zise bytes
- *@nmemb: number of element in the array
- *@size: bytes for each position in the array
- *Return: pointer void
+ * string_nconcat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int i;
+	char *concat;
+	unsigned int len = n, index;
 
-	if (nmemb == 0 || size == 0)
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
 
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
+	len = 0;
 
-	for (i = 0; i < nmemb * size; i++)
-		p[i] = 0;
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
 
-	return (p);
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
 
+	concat[len] = '\0';
+
+	return (concat);
 }
--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,27 +1,31 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdlib.h>
+
 /**
- * array_range - allocates memory for an initialized buffer
- * @min: number of memory bytes
- * @max: number of data type bits
- * Return: return pointer to initialized buffer
+ * array_range - Creates an array of integers ordered
+ *               from min to max, inclusive.
+ * @min: The first value of the array.
+ * @max: The last value of the array.
+ *
+ * Return: If min > max or the function fails - NULL.
+ *         Otherwise - a pointer to the newly created array.
  */
 int *array_range(int min, int max)
 {
-	int *p;
-	unsigned int i;
+	int *array, index, size;
 
 	if (min > max)
 		return (NULL);
 
-	p = malloc(sizeof(int) * (max - min + 1));
-	if (p == NULL)
+	size = max - min + 1;
+
+	array = malloc(sizeof(int) * size);
+
+	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; min <= max ; i++)
-	{
-		p[i] = min;
-		min++;
-	}
+	for (index = 0; index < size; index++)
+		array[index] = min++;
 
-	return (p);
+	return (array);
 }
