80313e7972eb174e6c6ebf2f290a42739376b32c
GuillermoRosario <guillermorosario628@gmail.com>
2022-11-01 16:18:17 +0000

    Dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * init_dog - .
+ * @d: ..
+ * @name: ...
+ * @age: .
+ * @owner: ..
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+	}
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
