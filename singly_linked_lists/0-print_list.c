a17ed81125ce336b2e2168ad7bf207df9ef69090
GuillermoRosario <guillermorosario628@gmail.com>
2022-11-15 21:26:10 +0000

    printlist

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,27 +1,26 @@
 #include "lists.h"
 /**
- * print_list - function that prints all the elementes of a list_t list.
- * @h: pointer to the start of the linked list
- * Return: the number of nodes */
-
+ * print_list - prints elements of a list
+ * @h: parameter
+ * Return: the numbers of the node
+ */
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	int i = 0;
 
 	while (h != NULL)
 	{
-		printf("[%u] ", h->len);
-		if (h->str == NULL)
-		{
-			printf("(nil)\n");
-		}
-		else
-		{
-			printf("%s\n", h->str);
-		}
-		h = h->next;
-		count += 1;
+		printf("[%d] ", h->len);
+		i++;
+	if (h->str == NULL)
+	{
+		printf("(nil)\n");
 	}
-
-	return (count);
+	else
+	{
+		printf("%s\n", h->str);
+	}
+		h = h->next;
+	}
+	return (i);
 }
