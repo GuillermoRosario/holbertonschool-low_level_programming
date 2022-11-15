54c8b8391f96351f2db96d55af417df631259b5d
GuillermoRosario <guillermorosario628@gmail.com>
2022-11-15 20:49:10 +0000

    printlists

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,11 +1,9 @@
 #include "lists.h"
-
 /**
  * print_list - Prints all the elements of a list_t list.
  * @h: Pointer to The start of the linked list.
  *
- * Return: The number of nodes in h.
- */
+ * Return: The number of nodes in h.*/
 
 size_t print_list(const list_t *h)
 
