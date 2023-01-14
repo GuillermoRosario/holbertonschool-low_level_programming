fb248494e7c994987e4fd60e1c0dec594153dd03
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-14 01:40:43 +0000

    identifier correction

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,7 +10,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i=0; i < size; i++)
 	{
-		printf ("Value checked array[%1d] = [%d}\n", i, array[i]);
+		printf ("Value checked array[%1ld] = [%d}\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
