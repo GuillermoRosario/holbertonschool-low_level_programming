18103f5a48d4f9158674d06a772b414b834be840
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-14 01:47:33 +0000

    betty correction

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -8,10 +8,10 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 
 	for ( i = 0; i < size; i++)
-	{
+{
 	printf ("Value checked array[%1ld] = [%d}\n", i, array[i]);
-		if(array[i] == value)
+		if (array[i] == value)
 		return (i);
-	}
+}
 	return (-1);
 }
