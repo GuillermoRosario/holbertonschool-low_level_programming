541d4aa0d8e3b388ed60a24b02419145290cdcfb
GuillermoRosario <guillermorosario628@gmail.com>
2023-01-14 01:44:36 +0000

    identifier correction

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,6 +1,5 @@
 
 #include "search_algos.h"
-
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
@@ -8,11 +7,11 @@ int linear_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	for (i=0; i < size; i++)
+	for ( i = 0; i < size; i++)
 	{
-		printf ("Value checked array[%1ld] = [%d}\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+	printf ("Value checked array[%1ld] = [%d}\n", i, array[i]);
+		if(array[i] == value)
+		return (i);
 	}
 	return (-1);
 }
